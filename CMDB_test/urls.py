537f63c00281f1e872cd57ff009835e61de9d896
ZouJay <loveh164500629@163.com>
2020-07-24 21:31:54 +0800

    dsa
    dsad

--- CMDB_test/urls.py
+++ CMDB_test/urls.py
@@ -3,7 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
-Function views
+Function viewsdsadas
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
